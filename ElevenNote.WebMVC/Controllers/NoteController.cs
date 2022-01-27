f030f926045f921a0ed87a1c8c8f4286e4915f26
Tristan Auer <tristanjauer@gmail.com>
2022-01-27 18:03:51 -0500

    Detail services and model

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -43,7 +43,14 @@ namespace ElevenNote.WebMVC.Controllers
             ModelState.AddModelError("", "Note could not be created");
             return View(model);
         }
+        
+        public ActionResult Details(int id)
+        {
+            var svc = CreateNoteService();
+            var model = svc.GetNoteById(id);
 
+            return View(model);
+        }
         private NoteService CreateNoteService()
         {
             var userId = Guid.Parse(User.Identity.GetUserId());
